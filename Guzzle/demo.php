3b5589b4f791fccfc45aa07486bf9c6384d81950
Terry.FromMac <tkstorm@163.com>
2018-02-05 13:23:29 +0800

    Guzzle Demo.

---
+++ Guzzle/demo.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Terry
+ * Date: 2018/2/5
+ * Time: 13:00
+ */
+
+define('ROOT_PATH', dir(__DIR__));
+
+require_once ROOT_PATH. 'vendor/autoload.php';
+
+class GuzzleDemo {
+
+
+
+    /**
+     * Guzzle Version 6
+     *
+     * GuzzleDemo constructor.
+     */
+    public function __construct()
+    {
+
+    }
+
+
+
+}
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-## demos
+### demos
 > 针对工作中的遇到一些问题以及相关技术调研分析前，做的一些简单事例；
 
-###
+### Practice List
 - Opcache的相关清理；
 - Flock的相关模拟；
 - Session Lock相关问题；
+- Guzzle
